24a91ae81d7f5c26bc5ec0d10924f2f6b6d3cd8b
varshithamannem <147411738+varshithamannem@users.noreply.github.com>
2023-10-18 14:42:21 +0530

    Add files via upload

---
+++ project/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template,request
+
+
+app=Flask  (__name__)
+
+
+@app.route('/')
+def helloworld():
+    return render_template("index.html")
+
+if __name__ =='__main__':
+    app.run(debug=False,port = 8000)
